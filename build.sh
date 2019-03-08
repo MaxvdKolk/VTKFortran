c4c722d4623ffe0ccf2e89a37de9e02dee25ebec
Max van der Kolk <m.vanderkolk@tudelft.nl>
2019-03-08 15:20:45 +0100

    linux

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+# build all files
 case "$OSTYPE" in 
         darwin*)
-        source /opt/intel/compilers_and_libraries_2019.2.184/mac/bin/compilervars.sh intel64
-        source /opt/intel/compilers_and_libraries_2019.2.184/mac/mkl/bin/mklvars.sh intel64
-        wait
+        path=/opt/intel/compilers_and_libraries_2019.2.184/mac
+        ;;
+        linux*)
+        path=/opt/ud/intel_xe_2019u2/compilers_and_libraries_2019.2.187/linux
+        ;;
+        *)
+        ;;
 esac
+
+source $path/bin/compilervars.sh intel64
+source $path/mkl/bin/mklvars.sh intel64
 
 make clean
 make
