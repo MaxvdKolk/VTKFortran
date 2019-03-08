a67391d0ed4c42a0c54a707532ae24bbd31cfc48
Max van der Kolk <m.vanderkolk@tudelft.nl>
2019-03-08 14:55:20 +0100

    switch case

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-source /opt/intel/compilers_and_libraries_2019.2.184/mac/bin/compilervars.sh intel64
-source /opt/intel/compilers_and_libraries_2019.2.184/mac/mkl/bin/mklvars.sh intel64
-wait
+case "$OSTYPE" in 
+        darwin*)
+        source /opt/intel/compilers_and_libraries_2019.2.184/mac/bin/compilervars.sh intel64
+        source /opt/intel/compilers_and_libraries_2019.2.184/mac/mkl/bin/mklvars.sh intel64
+        wait
+esac
 
 make clean
 make
